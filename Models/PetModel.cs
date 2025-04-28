1559a809e86a368454cf08a00611177b0c763526
JustKZ <kz5ee@yahoo.com>
2025-04-28 13:26:28 -0500

    Add pet model

---
+++ Models/PetModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mvp_refresher.Models
+{
+    public class PetModel
+    {
+        //Fields
+        private int id;
+        private string name;
+        private string type;
+        private string color;
+
+        //Properties
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public string Type { get => type; set => type = value; }
+        public string Color { get => color; set => color = value; }
+    }
+}
