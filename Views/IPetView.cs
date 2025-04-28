99c6e15dac5d9032d02c930d5dcf41e52cd499cf
JustKZ <kz5ee@yahoo.com>
2025-04-28 13:47:42 -0500

    Add view interface

---
+++ Views/IPetView.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mvp_refresher.Views
+{
+    public interface IPetView
+    {
+        //Properties - Fields
+        string PetId { get; set; }
+        string PetName { get; set; }
+        string PetType { get; set; }
+        string PetColor { get; set; }
+
+        string SearchValue { get; set; }
+        bool IsEditing { get; set; }
+        bool IsSuccessful { get; set; }
+        string Message { get; set; }
+
+        //Events
+        event EventHandler SearchEvent;
+        event EventHandler AddNewEvent;
+        event EventHandler EditEvent;
+        event EventHandler DeleteEvent;
+        event EventHandler SaveEvent;
+        event EventHandler CancelEvent;
+
+        //Methods
+        void SetPetListBindingSource(BindingSource petList);
+        void Show();
+    }
+}
