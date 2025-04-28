2dcbcadcd63bd9135225e02468ebff8118d016bd
JustKZ <kz5ee@yahoo.com>
2025-04-28 13:36:46 -0500

    Add validation to model

--- Models/PetModel.cs
+++ Models/PetModel.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace mvp_refresher.Models
 {
@@ -14,10 +16,23 @@ namespace mvp_refresher.Models
         private string type;
         private string color;
 
-        //Properties
+        //Properties - Validation
+        [DisplayName("Ped ID")]
         public int Id { get => id; set => id = value; }
+
+        [DisplayName("Ped Name")]
+        [Required(ErrorMessage = "Pet name is required.")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 50 characters.")]
         public string Name { get => name; set => name = value; }
+
+        [DisplayName("Ped Type")]
+        [Required(ErrorMessage = "Pet type is required.")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Type must be between 3 and 50 characters.")]
         public string Type { get => type; set => type = value; }
+
+        [DisplayName("Ped Color")]
+        [Required(ErrorMessage = "Pet color is required.")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Color must be between 3 and 50 characters.")]
         public string Color { get => color; set => color = value; }
     }
 }
