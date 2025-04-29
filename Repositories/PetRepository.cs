ec1fbfa5e2a2310d1272daf0f51170ff73f4e6b4
JustKZ <kz5ee@yahoo.com>
2025-04-29 14:08:56 -0500

    Add data things and make queries

---
+++ Repositories/PetRepository.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using Microsoft.Data;
+using mvp_refresher.Models;
+
+namespace mvp_refresher.Repositories
+{
+    public class PetRepository : BaseRepository, IPetRepository
+    {
+
+        //Constructor
+        public PetRepository(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        //Methods
+        public void Add(PetModel petModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(PetModel petModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<PetModel> GetAll()
+        {
+            var petList = new List<PetModel>();
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = "SELECT * FROM Pet ORDER BY Pet_Id DESC";
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var petModel = new PetModel();
+                        petModel.Id = (int)reader[0];
+                        petModel.Name = reader[1].ToString();
+                        petModel.Type = reader[2].ToString();
+                        petModel.Color = reader[3].ToString();
+                        petList.Add(petModel);
+                    }
+                }
+            }
+
+            return petList;
+        }
+
+        public IEnumerable<PetModel> GetByValue(string value)
+        {
+            var petList = new List<PetModel>();
+            using (var connection = new SqlConnection(connectionString))
+            using (var command = new SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = @"SELECT * FROM Pet 
+                                                                 WHERE Pet_Id=@id or Pet_Name LIKE @name+'%'
+                                                                 ORDER BY Pet_Id DESC";
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var petModel = new PetModel();
+                        petModel.Id = (int)reader[0];
+                        petModel.Name = reader[1].ToString();
+                        petModel.Type = reader[2].ToString();
+                        petModel.Color = reader[3].ToString();
+                        petList.Add(petModel);
+                    }
+                }
+            }
+
+            return petList;
+        }
+    }
+}
